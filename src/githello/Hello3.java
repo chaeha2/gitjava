969ac11dd1ff70d70be8330bd5f8bd29abe5ff28
chaeha <echaeha@gmail.com>
2021-07-08 12:39:43 +0900

    hello3추가 hello 파일에는 요일에 관한 정보 및 안부인사

--- src/githello/Hello3.java
+++ src/githello/Hello3.java
@@ -1,10 +1,10 @@
 package githello;
 
-public class Hello2 {
+public class Hello3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello2-project");
+		System.out.println("hello3");
 	}
 
 }
